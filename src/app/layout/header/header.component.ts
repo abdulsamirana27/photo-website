50a1ff62a35445746a5295c046730876e4b7b312
nabeel.hafiz <nabeel.hafiz@axabiztech.com>
2023-02-28 17:07:09 +0500

    header updated

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Document} from "postcss";
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   value?: Date;
+
   constructor() { }
 
   ngOnInit(): void {
