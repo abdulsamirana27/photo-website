3760ce7a819fdfe8c2acc58f2dc41af36529915c
nabeel.hafiz <nabeel.hafiz@axabiztech.com>
2023-03-02 15:33:51 +0500

    changes

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../../services/productservice";
+import {Product} from "../../models/product";
 
 @Component({
   selector: 'app-header',
@@ -7,12 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   value?: Date;
+  responsiveOptions;
+  products: Product[] = [];
 
-  constructor() {
-
+  constructor(private productService: ProductService) {
+    this.responsiveOptions = [
+      {
+        breakpoint: '1024px',
+        numVisible: 3,
+        numScroll: 3
+      },
+      {
+        breakpoint: '768px',
+        numVisible: 2,
+        numScroll: 2
+      },
+      {
+        breakpoint: '560px',
+        numVisible: 1,
+        numScroll: 1
+      }
+    ];
   }
 
   ngOnInit() {
+    this.productService.getProductsSmall().then(products => {
+      this.products = products;
+    });
   }
 
 }
